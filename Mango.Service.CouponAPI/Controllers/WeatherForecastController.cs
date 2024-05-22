349c1d5084a2fb46104597d4717d973b7c00ccbb
tariq,nabeel <tariqnabeel222@gmail.com>
2024-05-22 16:32:44 +0300

    adding s in weather controller

--- Mango.Service.CouponAPI/Controllers/WeatherForecastController.cs
+++ Mango.Service.CouponAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Mango.Service.CouponAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezings", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
